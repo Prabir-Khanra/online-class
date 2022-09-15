ab4b422eb21b53882ecb35816f884b3b216724e0
Prabir Khanra <prabir123khanra@gmail.com>
2022-09-15 11:22:57 +0530

    operators and type casting

---
+++ comp.py
@@ -0,0 +1,31 @@
+x = 12
+y = 3
+add = 12 +3
+sub = 12 - 3
+multi = 12 * 3 
+div = 12 / 3
+mod = 12 % 3
+print(add) # answer ...15
+print(sub) # answer ...9
+print(multi) # answer ... 36
+print(div) # answer ... 4.0
+print(mod) # answer ... 0
+
+'''
+a = 345
+b = 398
+c = 398
+value1 = 'DEV'
+value2 = "DEV"
+a < b    # answer ..... true
+a > b    # answer ..... false
+a <= b    # answer ..... true
+a => b    # answer ..... false
+a != b    # answer ..... true
+a == b    # answer ..... false
+b == c    # answer ..... true
+value2 == value1    # answer ..... true
+
+print(a < b) # answer ...... true
+
+'''
---
+++ type_cast.py
@@ -0,0 +1,57 @@
+# type casting
+
+# first
+a = '345.6' # string => float => int
+b = float(a) # converted to float value....answere is 345.6
+c = int(b) # converted to int value....answere is 345
+print(type(b))
+print(type(c))
+print(b)
+print(c)
+
+
+
+#second
+p = '34' # string => int
+q = int(p) #  # converted to int value....answere is 34
+r = float(q) # converted to float value....answere is 34.0
+print(type(q))
+print(type(r))
+print(q)
+print(r)
+
+
+
+#third
+s = 34 #int => string
+t = str(s) # converted to string value....answere is '34'
+u = float(s) # converted to float value....answere is 34.0
+print(type(t))
+print(type(u))
+print(t)
+print(u)
+
+
+
+#fourth
+v = 34.0 # float => int  
+w = int(v) # converted to float value....answere is 34
+x = str(v) # converted to string value....answere is '34.0'
+print(type(w))
+print(type(x))
+print(w)
+print(x)
+
+
+
+
+#five
+# error ==================== error ==================== error ===================
+k = 'Ranvir Kapoor' # string => int
+l = int(k) # converted to float value ....... error...we cant convert a text value into an int value
+m = float(k) # converted to float value ....... error...we cant convert a text value into an float value
+print(type(l))
+print(type(m))
+# ==============================================================================
+
+
