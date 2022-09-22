e38f93b83c09b51a85cc1833c989b214717fc10c
Prabir Khanra <prabir123khanra@gmail.com>
2022-09-22 09:03:22 +0530

    function

---
+++ func.py
@@ -0,0 +1,72 @@
+#in python, function is defined using def
+
+# ============================================================================================
+# first ======================================================================================
+#creating a funciton
+def myName () :
+    print("Hello Programmer")
+# call a funciton
+myName()
+
+
+
+
+
+# ============================================================================================
+# second =====================================================================================
+def myAddress (x): # Here 'x' is a parameter
+    print("My address: "+x)
+
+myAddress("Tamluk") # Here 'Tamluk is an argument'
+
+
+
+
+
+
+
+# ============================================================================================
+# third ======================================================================================
+# This function expects 2 arguments, but gets only 1
+def showSomething(name,age):
+    print("Show something")
+
+showSomething("Prabir")
+# error ===================
+
+
+
+
+
+
+
+# ============================================================================================
+# fourth ======================================================================================
+# if your function expects 2 arguments, you have to call the function with 2 arguments, not more, and not less.
+def showSomething(name,age):
+    print("Show something")
+
+showSomething("Prabir",25)
+
+
+
+
+
+
+
+# ============================================================================================
+# fifth ======================================================================================
+# if you for some reason have a function definition with no content, put in the pass statement to avoid getting an error.
+
+def myProffession ():
+    print("I am a Mobile Application Developer")
+
+def myHobby ():
+    pass
+
+myProffession()
+myHobby()
+
+
+
+
---
+++ task.py
@@ -0,0 +1,21 @@
+# ===========================================================================================================
+# ===========================================================================================================
+# send first name and last name to a function as arguments
+# print the value in the function 
+
+
+
+
+
+
+
+# ===========================================================================================================
+# ===========================================================================================================
+name = "Arman Mallik"
+
+#use if-else
+#print using function
+
+# if length of name variable is less than 10, print 'at least 10 letters needed'
+# if length of name variable is greater than 20, print 'too many letters'
+# else print 'Thank you'
