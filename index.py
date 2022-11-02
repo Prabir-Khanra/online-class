27b270bdb7929724449d53987f05a505b6bb22a9
Prabir Khanra <prabir123khanra@gmail.com>
2022-11-02 13:08:38 +0530

    list in python

--- index.py
+++ index.py
@@ -7,14 +7,23 @@ list3 = [True, False, False,True,True] # contains 5 items
 print(name)
 
 
+
+
+
 # ====== accessing list item 
 x = name[1] # accessing seecond item from name list 
 print(x)
 
 
+
+
+
 # ====== update or change list item 
 name[1] = "Hrittik Roshan"  # change second item from name list 
 print(name)
+
+
+
 
 
 # ====== add list item 
@@ -23,6 +32,9 @@ super_star = ["Katrina Kaif","Disha Patani","Pooja Hegde","Kriti Sanon"]
 super_star.append("Sradhya Kapoor") # item will be added in the last position
 #super_star.insert(1,"Urvashi Rautela") # item will be added in the second position
 print(super_star)
+
+
+
 
 
 # ====== remove list item 
@@ -34,12 +46,14 @@ super_star.remove("Disha Patani")
 print(super_star)
 
 
+
+
+
 # ====== add two lists
 heros = ["Shahrukh Khan","Akshay Kumar","Hrittik Roshan"]
 heroines = ["Katrina Kaif","Disha Patani","Pooja Hegde","Kriti Sanon"]
 heros.extend(heroines) # here we added heroines list to heros list
 print(heros)
-
 # list2 = heros + heroines
 # print(list2)
 
